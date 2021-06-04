efe62138ecc3c0d64d8bdbf99ed2c00ec5cb4f1c
alexanderjervis <alexander.jervis@revature.net>
2021-06-04 11:41:49 -0400

    moved docstring into function block

--- writer.py
+++ writer.py
@@ -1,9 +1,8 @@
-
-"""open and write to the file located at path
+def write_css(path, css):
+    """open and write to the file located at path
 get the selector and props items from css reader
 write in the values from css reader to the file 
 and using selector and props items for formatting."""
-def write_css(path, css):
     with open(path, 'w') as file:
         for selector, props in css.items():
             file.write(f'{selector} {{\n')
