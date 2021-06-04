fe1340c6a839dca38a6c1991bc29ebfef798d2c6
alexanderjervis <alexander.jervis@revature.net>
2021-06-04 11:02:58 -0400

    annotate writer.py

--- writer.py
+++ writer.py
@@ -1,4 +1,8 @@
 
+#open and write to the file located at path
+#get the selector and props items from css reader
+#write in the values from css reader to the file 
+#and using selector and props items for formatting.
 def write_css(path, css):
     with open(path, 'w') as file:
         for selector, props in css.items():
