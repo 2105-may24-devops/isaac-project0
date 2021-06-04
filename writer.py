c968bd25a042ec9dc22a7eb5036cc47b948b54cb
Isaac Galang <isaac.t.galang@gmail.com>
2021-06-04 10:05:42 -0500

    Added a docstring to writer.write_css()

--- writer.py
+++ writer.py
@@ -1,5 +1,19 @@
 
 def write_css(path, css):
+    '''Writes a .css file from the output of reader.construct()
+    
+    ---
+    path:
+        Path to the .css file to be written
+        
+    css:
+        Dictionary of css to be written.
+        Syntax:
+            {
+                'selector': [('property1', 'value1'), ('prop2', 'val2')],
+                'sel2': [('prop3', 'val3')]
+            }
+    '''
     with open(path, 'w') as file:
         for selector, props in css.items():
             file.write(f'{selector} {{\n')
