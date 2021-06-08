45aa2b0612da04e3dbe2ab6c82cc2c18d2020c3b
Isaac Galang <isaac.t.galang@gmail.com>
2021-06-08 16:38:10 +0000

    Added a shell script to run some basic tests of all input methods

---
+++ ansible/tests.sh
@@ -0,0 +1,38 @@
+python3 p0.py output.css -f input.txt
+if cmp output.css test/output-txt.css; then
+	echo 'Plaintext test passed'
+else
+	echo 'Plaintext test failed'
+fi
+
+python3 p0.py output.css -x input.xml
+if cmp output.css test/output-xml.css; then
+	echo 'Basic XML test passed'
+else
+	echo 'Basic XML test failed'
+fi
+
+python3 p0.py output.css -x complicated.xml
+if cmp output.css test/output-complicated.css; then
+	echo 'Complex XML test passed'
+else
+	echo 'Complex XML test failed'
+fi
+
+cat input.txt | python3 p0.py output.css -i > /dev/null
+# Direct to /dev/null so we don't clutter our screen
+# with the syntax reminder that gets printed in an
+# interactive session
+if cmp output.css test/output-interactive.css; then
+	echo 'Interactive test passed'
+else
+	echo 'Interactive test failed'
+fi
+
+python3 p0.py output.css -c -p font-size 12px
+if cmp output.css test/output-cli.css; then
+	echo 'CLI test passed'
+else
+	echo 'CLI test failed'
+fi
+
