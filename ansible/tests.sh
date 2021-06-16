eecfd53897749041fc8122f0ba294276f943afc3
Isaac Galang <isaac.t.galang@gmail.com>
2021-06-16 09:36:38 -0500

    Updated tests.sh and inventory.yml to better work with azure devops pipelines.

--- ansible/tests.sh
+++ ansible/tests.sh
@@ -3,6 +3,7 @@ if cmp output.css test/output-txt.css; then
 	echo 'Plaintext test passed'
 else
 	echo 'Plaintext test failed'
+	exit 1
 fi
 
 python3 p0.py output.css -x input.xml
@@ -10,6 +11,7 @@ if cmp output.css test/output-xml.css; then
 	echo 'Basic XML test passed'
 else
 	echo 'Basic XML test failed'
+	exit 1
 fi
 
 python3 p0.py output.css -x complicated.xml
@@ -17,6 +19,7 @@ if cmp output.css test/output-complicated.css; then
 	echo 'Complex XML test passed'
 else
 	echo 'Complex XML test failed'
+	exit 1
 fi
 
 cat input.txt | python3 p0.py output.css -i > /dev/null
@@ -27,6 +30,7 @@ if cmp output.css test/output-interactive.css; then
 	echo 'Interactive test passed'
 else
 	echo 'Interactive test failed'
+	exit 1
 fi
 
 python3 p0.py output.css -c -p font-size 12px
@@ -34,5 +38,6 @@ if cmp output.css test/output-cli.css; then
 	echo 'CLI test passed'
 else
 	echo 'CLI test failed'
+	exit 1
 fi
 
